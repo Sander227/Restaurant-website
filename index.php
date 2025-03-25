75333271e6a780e4d83c1b133d6d17dda9e84295
Sander227 <1211888@student.roc-nijmegen.nl>
2025-03-25 13:13:26 +0100

    Created a reposatory

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE php>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<header>
+    <div class="header-cont">
+        <img src="images/Restaurantlogo.webp" class="restaurant-logo" alt="Het logo van het restaurant">
+        <a href="index.html" class="naam-doos">
+            <span class="naam-restaurant">Dough & Fire</span>
+        </a>
+    </div>
+
+    <div class="menu-bar">
+        <a href="index.html">
+            <span class="menu-text">Home</span>
+        </a>
+        <a href="menu.html">
+            <span class="menu-text">Menu</span>
+        </a>
+        <a href="contact.html">
+            <span class="menu-text">Contact</span>
+        </a>
+        <a href="reserveren.html">
+            <span class="menu-text">Reververen</span>
+        </a>
+    </div>
+
+    <div class="header-cont">
+        <button class="login">
+            <span>Login</span>
+        </button>
+    </div>
+</header>
+
+<body>
+    <div class="restaurant-plaatje">
+        <img src="images/Restaurantlogo.webp" class="home-pagina-logo" alt="Logo van restaurant">
+    </div>
+
+    <div class="container-over-dough-and-fire">
+        <div class="over-dough-and-fire">
+            <h2>Dough & Fire heeft de beste en lekkerste pizza's. Heeft ook het aardigste personeel en ook nog eens
+                snelle service. Wij hebben een mooi en chique restaurant. Kom gezellig eten bij Dough & Fire!
+            </h2>
+            <div class="reserveren">
+            <h2>
+                Reserveren
+            </h2>
+            <img src="images/Geel-pijltje.png" class="geel-pijltje-naar-rechts" alt="geel pijltje naar rechts">
+            </div>
+            
+        </div>
+        <img src="images/classic-cheese-pizza-recipe-2-64429a0cb408b.avif" class="pizza-plaatje"
+            alt="plaatje van een heerlijke pizza">
+    </div>
+</body>
+
+</html>
